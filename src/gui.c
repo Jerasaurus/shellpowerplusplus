6a3805e565d2c50e0155aa29616e70ff66dc777a
Jeremy <69064548+Recontaminated@users.noreply.github.com>
2025-12-20 17:46:14 +0900

    CI WORKS??

--- src/gui.c
+++ src/gui.c
@@ -2,12 +2,14 @@
  * GUI implementation using raygui
  */
 
- #ifdef _WIN32
- #define WIN32_LEAN_AND_MEAN
- #define NOGDI
- #define NOUSER
- #include <windows.h>
- #include <commdlg.h>
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+// Note: Do NOT define NOGDI or NOUSER here - commdlg.h needs them
+#include <windows.h>
+#include <commdlg.h>
+// Undefine Windows macros that conflict with raylib
+#undef near
+#undef far
 #endif
 
 #include "raylib.h"
